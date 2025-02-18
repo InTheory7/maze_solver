87516c4eac9d75a16d78403ea73a4562272d30c1
Joshua - Laptop <joshuacollins999@gmail.com>
2025-02-18 16:46:12 +0000

    Added a maze class, generated the cells, and drew them

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from window_class import Window
 from point_and_line import Point, Line
 from cell import Cell
+from maze import Maze
 
 def main():
     # Create window
@@ -20,21 +21,29 @@ def main():
     # win.draw_line(line3,"green")
 
     # Draw some cells:
-    x1, y1 = (10, 10)
-    x2, y2 = (110,110)
-    x3, y3 = (210,210)
-
-    cell1 = Cell(win)
-    cell1.bottom_wall = False
-    cell2 = Cell(win)
-    cell2.top_wall = False
-    cell2.left_wall = False
-    cell1.draw(x1, y1, x2, y2)
-    cell2.draw(x2, y2, x3, y3)
-
-    # Draw a line between the two cells:
-    # cell1.draw_move(cell2)        # Red
-    cell1.draw_move(cell2, True)    # Grey
+    # x1, y1 = (10, 10)
+    # x2, y2 = (110,110)
+    # x3, y3 = (210,210)
+    # 
+    # cell1 = Cell(win)
+    # cell1.bottom_wall = False
+    # cell2 = Cell(win)
+    # cell2.top_wall = False
+    # cell2.left_wall = False
+    # cell1.draw(x1, y1, x2, y2)
+    # cell2.draw(x2, y2, x3, y3)
+    # 
+    # # Draw a line between the two cells:
+    # # cell1.draw_move(cell2)        # Red
+    # cell1.draw_move(cell2, True)    # Grey
+     
+    # Create a maze:
+    x1, y1 = (5,5)
+    num_rows = 5
+    num_cols = 5
+    cell_size_x = 40
+    cell_size_y = 40
+    maze = Maze(x1, y1, num_rows, num_cols, cell_size_x,cell_size_y,win)
 
     # Wait for window to close
     win.wait_for_close()
---
+++ maze.py
@@ -0,0 +1,45 @@
+from cell import Cell
+from time import sleep
+
+class Maze():
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x,cell_size_y,win):
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
+
+        self._create_cells()
+
+    def _create_cells(self):
+        # Initialize the maze of cells:
+        self._cells = []
+        for col in range(0,self._num_cols):
+            row_of_cells = []
+            for row in range(0,self._num_rows):
+                row_of_cells.append(Cell(self._win))
+            self._cells.append(row_of_cells)
+        # Draw each cell:
+        for col in range(0,self._num_cols):
+            for row in range(0,self._num_rows):
+                self._draw_cell(col, row)
+        
+    def _draw_cell(self, i, j):
+        # Cell class draw() method requires x-y
+            # co-ords of tl and br corners:
+        tl_x = self._x1 + (i * self._cell_size_x)
+        tl_y = self._y1 + (j * self._cell_size_y)
+        br_x = self._x1 + ((i+1) * self._cell_size_x)
+        br_y = self._y1 + ((j+1) * self._cell_size_y)
+        # Draw the cell:
+        self._cells[i][j].draw(tl_x,tl_y,br_x,br_y)
+        # Call the animate cell:
+        self._animate()
+
+    def _animate(self):
+        # Redraw the window and wait a fraction of a second before moving on:
+        self._win.redraw()
+        sleep(0.05)
+
